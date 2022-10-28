b4bb6c143219f56b2b9be3eaef66a750f67743b2
kasongoj <Joseph.Kasongo@sckans.edu>
2022-10-28 07:59:54 -0500

    docs affected

---
+++ docs/precache-manifest.a6806153b461b6df423024caffbd31ac.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "561af27de4361d847d494433a87275e0",
+    "url": "/open-react-template/index.html"
+  },
+  {
+    "revision": "ad0eaba3713b68f34510",
+    "url": "/open-react-template/static/css/main.c194263a.chunk.css"
+  },
+  {
+    "revision": "a6ab65b5c229481aa613",
+    "url": "/open-react-template/static/js/2.16b75209.chunk.js"
+  },
+  {
+    "revision": "d5b473ff893d6b5fac7d0b5081a5072a",
+    "url": "/open-react-template/static/js/2.16b75209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad0eaba3713b68f34510",
+    "url": "/open-react-template/static/js/main.62fe9c37.chunk.js"
+  },
+  {
+    "revision": "c4dc4e38c283d01c9aff",
+    "url": "/open-react-template/static/js/runtime-main.6ef72138.js"
+  },
+  {
+    "revision": "3ce357e71a836a86691b904396a8955c",
+    "url": "/open-react-template/static/media/cta-illustration.3ce357e7.svg"
+  },
+  {
+    "revision": "dfedae3fce5cc3e9ab607d4ddeaa64ec",
+    "url": "/open-react-template/static/media/icon1.dfedae3f.svg"
+  },
+  {
+    "revision": "aef85fd62e096500f4303006d517d10a",
+    "url": "/open-react-template/static/media/icon2.aef85fd6.svg"
+  },
+  {
+    "revision": "da6e7acc84b2e87956beaa8b651e57e8",
+    "url": "/open-react-template/static/media/icon3.da6e7acc.svg"
+  },
+  {
+    "revision": "b7a682eef4395436a0012bc1db7f86d0",
+    "url": "/open-react-template/static/media/icon4.b7a682ee.svg"
+  },
+  {
+    "revision": "937c6717eeb645e82ca005f192c07d6b",
+    "url": "/open-react-template/static/media/icon5.937c6717.svg"
+  },
+  {
+    "revision": "f9de8f26511eede7bc74a4a29af8a6fc",
+    "url": "/open-react-template/static/media/icon6.f9de8f26.svg"
+  },
+  {
+    "revision": "ea6949039d6554ebf2cf109f333f5811",
+    "url": "/open-react-template/static/media/illustration-section-01.ea694903.svg"
+  },
+  {
+    "revision": "63d0555d90c053eea1a1a7469d004661",
+    "url": "/open-react-template/static/media/illustration-section-02.63d0555d.svg"
+  },
+  {
+    "revision": "9168a0a2241126e2b35568e119290cd5",
+    "url": "/open-react-template/static/media/pic1.9168a0a2.png"
+  },
+  {
+    "revision": "26fd2ef2e1d30b2372fa7680f374715a",
+    "url": "/open-react-template/static/media/pic2.26fd2ef2.png"
+  },
+  {
+    "revision": "543bfcff4fe0d2df104938c4954d1568",
+    "url": "/open-react-template/static/media/pic3.543bfcff.png"
+  },
+  {
+    "revision": "a622fc5dd8adcdd27ea42218385013b4",
+    "url": "/open-react-template/static/media/video-placeholder.a622fc5d.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.353ed994c2e193026b35b721b6885bb5.js"
+  "/open-react-template/precache-manifest.a6806153b461b6df423024caffbd31ac.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/open-react-template/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
